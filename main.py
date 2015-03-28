d689f466705defdecb231f25eff57fbbdd6ff984
Pratheek Adidela <apratheek@gmail.com>
2015-03-28 18:50:49 +0530

    Created ByteArray and added DocStrings for all data types

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from mutables import *
+
+dependencyGraph = {}						#Define a central dependency graph that holds all relations between mutables
+
+class List(List):
+	def onchange(self):
+		#Code for onchange methods follows
+
+class Dict(Dict):
+	def onchange(self):
+		#Code for onchange method follows
+
+class Set(Set):
+	def onchange(self):
+		#Code for onchange method follows
+
+class ByteArray(ByteArray):
+	def onchange(self):
+		#Code for onchange method follows
 
--- mutables.py
+++ mutables.py
@@ -46,21 +46,29 @@ class List(list):
 		super(List, self).remove(value)
 		self.onchange()
 
+	def reverse(self):
+		"""L.reverse() -- reverse *IN PLACE*"""
+		super(List, self).reverse()
+		self.onchange()
+
 	def __setitem__(self, key, value):
+		"""Set self[key] to value."""
 		#print("Set key:%s and value:%s"%(key,value))
 		super(List, self).__setitem__(key, value)
 		self.onchange()
 
 	def __delitem__(self, key):
+		"""Delete self[key]."""
 		#print("Deleted key:%s"%key)
 		super(List, self).__delitem__(key)
 		self.onchange()
 
 	def onchange(self):
+		"""Internal method that is always called when there's a change in the data type"""
 		pass
 
 	#**************************************************************************************************
-	#Reverse isn't required, because this will be an Observable
+	#Reverse will also be an Observable
 	#**************************************************************************************************
 
 	#**************************************************************************************************
@@ -68,8 +76,8 @@ class List(list):
 	#**************************************************************************************************
 
 class Set(set):
-	"""set() -> new empty set object
- 	   set(iterable) -> new set object
+	"""Set() -> new empty set object
+ 	   Set(iterable) -> new set object
  	   Build an unordered collection of unique elements."""
 
 	def add(self, value):
@@ -107,6 +115,7 @@ class Set(set):
 		self.onchange()
 
 	def onchange(self):
+		"""Internal method that is always called when there's a change in the data type"""
 		pass
 
 	#**************************************************************************************************
@@ -145,37 +154,59 @@ class Int(int):
 	#	super(Int, self).__init__(value)
 
 class Dict(dict):
+	"""Dict() -> new empty dictionary
+ 	   Dict(mapping) -> new dictionary initialized from a mapping object's
+        (key, value) pairs
+ 	   Dict(iterable) -> new dictionary initialized as if via:
+ 	      d = Dict()
+ 	      for k, v in iterable:
+ 	          d[k] = v
+ 	  Dict(**kwargs) -> new dictionary initialized with the name=value pairs
+ 	      in the keyword argument list.  For example:  Dict(one=1, two=2)"""
+	
 	def clear(self):
+		"""D.clear() -> None.  Remove all items from D."""
 		super(Dict, self).clear()
 		self.onchange()
 		
 
 
 	def pop(self, key):
+		""" D.pop(k[,d]) -> v, remove specified key and return the corresponding value.
+ 		      If key is not found, d is returned if given, otherwise KeyError is raised."""
 		print(super(Dict, self).pop(key))
 		self.onchange()
 		
 
 	def popitem(self):
+		"""D.popitem() -> (k, v), remove and return some (key, value) pair as a
+ 		      2-tuple; but raise KeyError if D is empty."""
 		print(super(Dict, self).popitem())
 		self.onchange()
 
 	def update(self, anotherDict):
+		"""D.update([E, ]**F) -> None.  Update D from dict/iterable E and F.
+ 	      If E is present and has a .keys() method, then does:  for k in E: D[k] = E[k]
+ 	      If E is present and lacks a .keys() method, then does:  for k, v in E: D[k] = v
+ 	      In either case, this is followed by: for k in F:  D[k] = F[k]"""
 		super(Dict, self).update(anotherDict)
 		self.onchange()
 		
 
 	def __setitem__(self, key, value):
-		print("Set key:%s and value:%s"%(key,value))
+		"""Set self[key] to value."""
+		#print("Set key:%s and value:%s"%(key,value))
 		super(Dict, self).__setitem__(key, value)
 		self.onchange()
 
 	def __delitem__(self, key):
-		print("Deleted key:%s"%key)
+		"""Delete self[key]."""
+		#print("Deleted key:%s"%key)
 		super(Dict, self).__delitem__(key)
 		self.onchange()
 
 	def onchange(self):
+		"""Internal method that is always called when there's a change in the data type"""
 		pass
 
 
@@ -183,3 +214,87 @@ class Dict(dict):
 
 
 ######Need to add methods for bytearray here
+
+class ByteArray(bytearray):
+	"""ByteArray(iterable_of_ints) -> ByteArray
+ 		  ByteArray(string, encoding[, errors]) -> ByteArray
+ 	  	ByteArray(bytes_or_buffer) -> mutable copy of bytes_or_buffer
+ 	  	ByteArray(int) -> bytes array of size given by the parameter initialized with null bytes
+ 	  	ByteArray() -> empty bytes array
+ 	  
+ 	  	Construct an mutable bytearray object from:
+ 	    	- an iterable yielding integers in range(256)
+ 	    	- a text string encoded using the specified encoding
+ 	    	- a bytes or a buffer object
+ 	    	- any object implementing the buffer API.
+ 	    	- an integer"""
+	
+	def __delitem__(self, key):
+		"""Delete self[key]."""
+		super(ByteArray, self).__delitem__(key)
+		self.onchange()
+
+	def __setitem__(self, key, value):
+		"""Set self[key] to value."""
+		super(ByteArray, self).__setitem__(key, value)
+		self.onchange()
+
+	def append(self, value):
+		"""B.append(int) -> None
+ 	      
+ 	      	Append a single item to the end of B."""
+		super(ByteArray, self).append(value)
+		self.onchange()
+
+	def clear(self):
+		"""B.clear() -> None
+       
+ 		    Remove all items from B."""
+		super(ByteArray, self).clear()
+		self.onchange()
+
+	#**************************************************************************************************
+	#Count again is an Observable here, Decode, Reverse too. Have reverse as a method as well as an observable parameter
+	#**************************************************************************************************
+
+	def extend(self, itr):
+		"""B.extend(iterable_of_ints) -> None
+       
+ 	      Append all the elements from the iterator or sequence to the
+ 	      end of B."""
+		super(ByteArray, self).extend(itr)
+		self.onchange()
+
+	def insert(self, index, value):
+		"""B.insert(index, int) -> None
+       
+ 	      Insert a single item into the bytearray before the given index."""
+		super(ByteArray, self).insert(index, value)
+		self.onchange()
+
+	def pop(self, location=-1):
+		"""B.pop([index]) -> int
+       
+ 	      Remove and return a single item from B. If no index
+ 	      argument is given, will pop the last value."""
+		super(ByteArray, self).pop(location)
+		self.onchange()
+
+	def remove(self, value):
+		"""B.remove(int) -> None
+       
+ 	      Remove the first occurrence of a value in B."""
+		super(ByteArray, self).remove(value)
+		self.onchange()
+
+	def reverse(self):
+		"""B.reverse() -> None
+       
+ 	      Reverse the order of the values in B in place."""
+		super(ByteArray, self).reverse()
+		self.onchange()
+
+
+	def onchange(self):
+		"""Internal method that is always called when there's a change in the data type"""
+		pass
